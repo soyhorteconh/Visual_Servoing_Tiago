2cd28e356635b5e104cdeae9904044a965c9f658
soyhorteconh <a01750150@tec.mx>
2023-10-17 15:06:16 +0200

    robot hand pose

---
+++ apriltags_ws/src/inria_maths/src/Polytope.cpp
@@ -0,0 +1,13 @@
+#include <inria_maths/Polytope.hpp>
+
+namespace inria {
+
+/**
+ * Explicit template instantiation
+ */
+template class Polytope<2>;
+template class Polytope<3>;
+template class Polytope<6>;
+
+}
+
